cfdb46ef4fab9e3a6d7e185dae1b7483a84de268
bakken1 <habbo_og_obbahfan@hotmail.com>
2018-02-15 16:58:22 +0100

    Update files

--- schema/items/40kbooks/sh_hehe.lua
+++
@@ -1,4 +0,0 @@
-ITEM.name = "DON'T READ ME" -- In game name
-ITEM.desc = "" -- In game description
-ITEM.price = 0 -- Price in shop, keep this at 0
-ITEM.url = "https://www.pornhub.com"
--- schema/sh_models.lua
+++
@@ -1,7 +0,0 @@
-
--- Clone Troopers
-nut.anim.setModelClass("models/gonzo/swbf2phase1/unassigned/unassigned.mdl", "player")
-
-
-msg = "Loaded sh_models.lua successfully."
-print(msg)
