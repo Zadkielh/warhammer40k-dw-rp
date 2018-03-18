92d39230063def413c1dee9b95fa1b4083306649
bakken1 <habbo_og_obbahfan@hotmail.com>
2018-03-18 22:22:52 +0100

    fixx

--- schema/factions/sh_dw_sergeant.lua
+++ schema/factions/sh_dw_sergeant.lua
@@ -13,7 +13,6 @@ function FACTION:onSpawn(client)
 		client:SetArmor(self.armor) -- Sets armour
 		client:SetMaxHealth(self.maxhealth) -- Sets maxhealth, that means the health you can be healed to.
 		client:SetHealth(self.health) -- Sets your health, you can not be healed to this amount unless your maxhealth is the same. This is needed because gmod sets your health to 100 by default.
-		client:SetModelScale(1.36, 0)
 end
 
 FACTION.pay = 20
